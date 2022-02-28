4e3fcdf992ae295cd9e4f0cb85b01dab89a5dbab
Marco A. Miranda S <marco2015.sandoval@gmail.com>
2022-02-28 12:02:29 -0300

    :construction: Menu on Navbar in progress

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,21 +1,31 @@
 import Link from 'next/link'
 import { useEffect, useRef, useState } from 'react'
 import { ButtonIcon } from '@/components/ButtonIcon'
-import { ArrowSvg, MenuSvg, SearchSvg, DoveSvg } from '../Svg'
+import clsx from 'clsx'
+import { ArrowSvg, MenuSvg, SearchSvg, DoveSvg, CloseSvg } from '../Svg'
 
 export const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
   const [searchQuery, setSearchQuery] = useState('')
   const [inputState, setInputState] = useState(false)
   const inputRef = useRef()
 
   const hanldeInputState = () => setInputState(!inputState)
 
+  const hideMenu = () => setShowMenu(false)
+
+  useEffect(() => {
+    showMenu && window.addEventListener('click', hideMenu)
+
+    return () => window.removeEventListener('click', hideMenu)
+  }, [showMenu])
+
   useEffect(() => {
     inputState && inputRef.current.focus()
   }, [inputState])
 
   return (
-    <header className='h-12 bg-slate-700 w-full flex items-center justify-center border-b-2 border-gray-600'>
+    <header className='h-12 bg-slate-700 w-full items-center justify-center border-b-2 border-gray-600 relative'>
       <div className='container flex justify-between items-center h-full space-x-1'>
         {inputState
           ? (
@@ -47,9 +57,35 @@ export const Navbar = () => {
             />}
           {inputState
             ? <ButtonIcon svgComponent={<SearchSvg />} />
-            : <ButtonIcon svgComponent={<MenuSvg />} />}
+            : <ButtonIcon
+                svgComponent={<MenuSvg />}
+                onClick={() => { setShowMenu(true) }}
+              />}
         </div>
       </div>
+      <>
+        {showMenu && <div className='w-full bg-gray-500 bg-opacity-50 h-screen absolute top-0' />}
+        <div className={clsx(
+          'w-full h-screen absolute top-0 flex items-start transition-all duration-300',
+          showMenu ? 'left-0' : '-left-full'
+        )}
+        >
+
+          <div className='w-full bg-slate-900 h-screen'>
+            <p>asd</p>
+          </div>
+          <div className={clsx(
+            'bg-slate-900 rounded-full m-2 transition-all',
+            showMenu ? 'scale-100 delay-300' : 'scale-0 delay-75'
+          )}
+          >
+            <ButtonIcon
+              svgComponent={<CloseSvg />}
+              onClick={hideMenu}
+            />
+          </div>
+        </div>
+      </>
     </header>
   )
 }
--- src/components/Svg/index.jsx
+++ src/components/Svg/index.jsx
@@ -27,3 +27,10 @@ export const ArrowSvg = (props) => (
     <path d='m4 12 1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z' />
   </svg>
 )
+
+export const CloseSvg = (props) => (
+  <svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path d='M0 0h24v24H0V0z' fill='none' />
+    <path d='M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z' />
+  </svg>
+)
