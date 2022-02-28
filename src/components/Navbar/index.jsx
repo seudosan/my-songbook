88fede87f019947479674109597c934a8cb6d906
Marco A. Miranda S <marco2015.sandoval@gmail.com>
2022-02-28 01:56:22 -0300

    :recycle: Navbar refactored

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,46 @@
-import { MenuSvg, SearchSvg, DoveSvg } from '../Svg'
+import Link from 'next/link'
+import { useEffect, useRef, useState } from 'react'
+import { ArrowSvg, MenuSvg, SearchSvg, DoveSvg } from '../Svg'
 
 export const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [inputState, setInputState] = useState(false)
+  const inputRef = useRef()
+
+  const hanldeInputState = () => setInputState(!inputState)
+
+  useEffect(() => {
+    inputState && inputRef.current.focus()
+  }, [inputState])
+
   return (
-    <header className='h-14 bg-slate-700 w-full flex items-center justify-center px-4'>
-      <div className='container flex justify-between fill-slate-200'>
-        <DoveSvg className=' h-9' />
-        <div className='flex items-center space-x-2'>
-          <SearchSvg className='h-7' />
+    <header className='h-12 bg-slate-700 w-full flex items-center justify-center px-4 border-b-2 border-gray-600'>
+      <div className='container flex justify-between fill-slate-200 space-x-3'>
+        {inputState
+          ? (
+            <button onClick={hanldeInputState}>
+              <ArrowSvg className='h-7 -rotate-90' />
+            </button>
+            )
+          : (
+            <Link href='/'>
+              <a>
+                <DoveSvg className='h-7' />
+              </a>
+            </Link>
+            )}
+        {inputState &&
+          <input
+            className='w-full h-9 bg-slate-900 text-slate-200 px-4 rounded-sm'
+            ref={inputRef}
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />}
+        <div className='flex items-center space-x-3'>
+          {!inputState &&
+            <button onClick={hanldeInputState}>
+              <SearchSvg className='h-7' />
+            </button>}
           <MenuSvg className='h-7' />
         </div>
       </div>
--- src/components/Svg/index.jsx
+++ src/components/Svg/index.jsx
@@ -20,3 +20,10 @@ export const MenuSvg = (props) => (
     <path d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' />
   </svg>
 )
+
+export const ArrowSvg = (props) => (
+  <svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path d='M0 0h24v24H0V0z' fill='none' />
+    <path d='m4 12 1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z' />
+  </svg>
+)
